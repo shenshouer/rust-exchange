9bf6c7f47ee07702660a1f3e4b237b4834c7a8f2
sope <shenshouer51@gmail.com>
2022-03-19 16:34:43 +0800

    update set default protobuf generate file

--- matching-engine/build.rs
+++ matching-engine/build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // tonic_build::compile_protos("proto/order.proto")?;
     tonic_build::configure()
-        .out_dir("src/order_protobuf")
+        // .out_dir("src/order_protobuf")
         .compile(&["proto/order.proto"], &["proto"])?;
     Ok(())
 }
